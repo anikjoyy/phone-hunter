9546a3ec67aca3fb76c8529728a7f6e8770a8498
anikjoyy <anikjoy.cse@gmail.com>
2022-03-02 02:55:41 +0600

    api added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
+const allMobiles = () => {
+  const searchValue = document.getElementById('search-box').value;
 
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
+  console.log(url);
+};
