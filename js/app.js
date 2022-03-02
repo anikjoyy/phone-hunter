7d0c71c993386cf66ead8321501693e359491558
anikjoyy <anikjoy.cse@gmail.com>
2022-03-02 17:08:45 +0600

    added details for single phone

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 arrow function for all phones from API
 ------------------------------------------- */
 const allPhones = () => {
+  // clearing value after showing the result
+  document.getElementById('search-phones').innerHTML = '';
+  document.getElementById('singlePhoneDetails').innerHTML = '';
+
+  // search button works
   const searchText = document.getElementById('search-box').value;
 
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
@@ -15,7 +20,12 @@ const allPhones = () => {
 arrow function for phone details
 --------------------------------------------------------- */
 const showPhoneDetails = (phones) => {
-  //console.log(phones);
+  //error checking for no result found
+  if (phones.length == 0) {
+    window.alert('No Mobile Founds!');
+    return;
+  }
+
   for (const data of phones) {
     const searchPhone = document.getElementById('search-phones');
     const div = document.createElement('div');
@@ -27,9 +37,44 @@ const showPhoneDetails = (phones) => {
                             <p class="card-text">${data.brand}</p>
                         </div>
                         <div class="card-footer">
-                            <button class="btn btn-success">Details</button>
+                            <button onclick="phoneDetails('${data.slug}')" class="btn btn-success">Details</button>
                         </div>
                     </div>`;
     searchPhone.appendChild(div);
   }
+};
+
+/* -------------------------------------------------------
+arrow function for showing phone details using slug
+----------------------------------------------------------- */
+const phoneDetails = (slug) => {
+  // console.log(slug);
+  const url = `https://openapi.programming-hero.com/api/phone/${slug}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displayPhoneFeatures(data.data));
+};
+
+/* -----------------------------------------------
+arrow function to show full features of a phone
+----------------------------------------------------- */
+const displayPhoneFeatures = (data) => {
+  document.getElementById('singlePhoneDetails').innerHTML = '';
+  const singlePhoneDetails = document.getElementById('singlePhoneDetails');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `<img src="${data.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${data.name}</h5>
+
+                    <p class="card-text">Sensors: ${data.mainFeatures.sensors}</p>
+
+                    <p class="card-text">WLAN: ${data.others.WLAN}</p>
+                    <p class="card-text">Bluetooth: ${data.others.Bluetooth}</p>
+                    <p class="card-text">GPS: ${data.others.GPS}</p>
+                    <p class="card-text">NFC: ${data.others.NFC}</p>
+                    <p class="card-text">Radio: ${data.others.Radio}</p>
+                    <p class="card-text">USB: ${data.others.USB}</p>
+                </div>`;
+  singlePhoneDetails.appendChild(div);
 };
